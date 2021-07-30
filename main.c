9152ffc5a494207b345f81c65c9f86d77f6d4f8c
Ammar Ratnani <ammrat13@gmail.com>
2021-07-30 16:10:26 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+
+    DDRB |= (1 << DDB5);
+
+    while(true) {
+        PORTB ^= (1 << PORTB5);
+        _delay_ms(1000);
+    }
+
+}
